63ebe63b0fcf29165594bec970885f2d078ab9e0
Ilmo van der Lowe <github@vanderlowe.com>
2013-03-04 12:21:13 +0000

    Removed unnecessary files

--- inst/example.r
+++
@@ -1,9 +0,0 @@
-localConfig()
-
-p <- getData(participants("age > 65", "gender = 1", "network_size"))
-a <- getData(address("current_location_city = 'New York'"))
-
-p[a, nomatch = 0]
-
-explain("address")
-variables("address")
