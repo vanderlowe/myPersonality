9aba550e734f3af6bd3daf7c1634302460f8f8a6
Ilmo van der Lowe <github@vanderlowe.com>
2013-03-05 10:09:19 +0000

    Included more tables for testing

--- R/myPersonality.R
+++ R/myPersonality.R
@@ -2,8 +2,8 @@ myPersonality <- function() {
   all.tables <- myPersonalitySQL("SHOW TABLES;")[,1]
   
   defined.funcs <- data.table(
-    table = c("demog", "address", "fb_employer"),
-    func =  c("participants", "adress", "employers")
+    table = c("demog", "address", "fb_employer", "swl", "fb_likes", "fb_user_likes"),
+    func =  c("participants", "adress", "employers", "satisfaction_with_life","facebook_likes", "participant_likes")
   )
   cat("Currently, the following functions are available:\n")
   
--- R/tableDefinitions.R
+++ R/tableDefinitions.R
@@ -12,3 +12,18 @@ employers <- function(...) {
   mySQL.table.name <- "fb_employer"
   return(createVirtualTable(mySQL.table.name, ...))
 }
+
+satisfaction_with_life <- function(...) {
+  mySQL.table.name <- "swl"
+  return(createVirtualTable(mySQL.table.name, ...))
+}
+
+facebook_likes <- function(...) {
+  mySQL.table.name <- "fb_like"
+  return(createVirtualTable(mySQL.table.name, ...))
+}
+
+participant_likes <- function(...) {
+  mySQL.table.name <- "fb_user_like"
+  return(createVirtualTable(mySQL.table.name, ...))
+}
