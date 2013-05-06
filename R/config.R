fa4b9de7f01d1033454e0a3d566b15645b7ed2a1
Ilmo van der Lowe <github@vanderlowe.com>
2013-05-06 16:10:55 +0100

    Check whether env vars already exist.

--- R/config.R
+++ R/config.R
@@ -12,9 +12,13 @@ config <- function() {
   Sys.setenv("myPersonality_host" = "alex.e-psychometrics.com")
   Sys.setenv("myPersonality_database" = "cpw_myPersonality")
   
-  uid <- readline("Please enter your user name: ")
-  Sys.setenv("myPersonality_user" = uid)
+  if (Sys.getenv("myPersonality_user") == "") {
+    uid <- readline("Please enter your user name: ")
+    Sys.setenv("myPersonality_user" = uid)
+  }
   
-  pwd <- readline("Please enter your password: ")
-  Sys.setenv("myPersonality_password" = pwd)
+  if (Sys.getenv("myPersonality_password") == "") {
+    pwd <- readline("Please enter your password: ")
+    Sys.setenv("myPersonality_password" = pwd)
+  }
 }
